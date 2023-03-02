74c5a485ef6ebf98ef2926163f320ef61148dc49
greatraksin <greatraksin@icloud.com>
2023-03-02 18:02:10 +0300

    Adding login menu

--- main.cpp
+++ main.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 
-int main() {
+// #### блок с функциями ###
+int menu();  // функция-кандидат (объявление функции)
+bool isLoggedIn(string *username, string password);
+bool userRegister(string *username, string *password, string *confirmation);
 
+int main() {
+    int option;
+    while (option != 0) {
+        option = menu();
+        switch (option) {
+            case 1:
+                cout << "Option 1" << endl;
+                break;
+            case 2:
+                cout << "Option 2" << endl;
+                break;
+        }
+    }
     return 0;
+}
+
+int menu() {
+    cout << "Choose option...\n";
+    cout << "0 - exit\n1 - Register\n2 - Login\n\n";
+    int ans;  // объявление переменной
+    cin >> ans;  // присвоение значения (инициализация)
+    return ans;
 }
 
 
